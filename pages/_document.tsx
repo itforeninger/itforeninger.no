4846c4520046272e242687c87ff166f6f7f7ce36
oleast <oleast@stud.ntnu.no>
2020-01-29 23:42:03 +0100

    Add server side support for styled-components, add base colors stylesheet

---
+++ pages/_document.tsx
@@ -0,0 +1,71 @@
+import Document, {
+  Head,
+  Html,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import { ComponentProps } from "react";
+import { ColorStyleSheet } from "../stylesheets/colors";
+
+const getInitialProps = async (
+  ctx: DocumentContext
+): Promise<DocumentInitialProps> => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () => {
+      return originalRenderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+      });
+    };
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      )
+    };
+  } finally {
+    sheet.seal();
+  }
+};
+
+type DocumentProps = ComponentProps<typeof Document>;
+
+const CustomDocument = (_: DocumentProps): JSX.Element => {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" type="image/png" href="/gallery-logo-256.png" />
+        <meta name="theme-color" content="#0D5474" />
+        <meta name="description" content="Forente IT-foreninger" />
+        <meta
+          name="keywords"
+          content="FIF, Forente IT-foreninger, itforeninger, student, it-student, studentforening"
+        />
+        <meta name="author" content="Forente IT-foreninger" />
+        <meta name="robots" content="index,follow" />
+        <link rel="canonical" href="https://itforeninger.no" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <body>
+        <ColorStyleSheet />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+CustomDocument.getInitialProps = getInitialProps;
+CustomDocument.renderDocument = Document.renderDocument;
+
+export default CustomDocument;
---
+++ stylesheets/colors.tsx
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components";
+
+/**
+ * Colors are defined as CSS variables for use to be able to change them in runtime.
+ * That also makes them work with media queries, like those for 'prefers-colors-scheme'.
+ */
+
+export const ColorStyleSheet = createGlobalStyle`
+  /* Colors */
+  --darkGreen: #246C5E;
+  --lightGray: #E7E7F0;
+  --beige: #FFF4EE;
+  --pink: #D7A1AC;
+  --orange: #FB7E4C;
+  --black: #050505;
+  --lightBlue: #ACCAFC;
+  --offBlack: #191F29;
+
+  /* Color uses */
+  --backgroundColor: var(--white);
+  --backgroundColor2: var(--beige);
+
+  /* Dark mode overrides */
+  @media (prefers-color-scheme: dark) {
+    --backgroundColor: var(--black);
+    --backgroundColor2: var(--offBlack); 
+  }
+`;
+
+/* TypeScript color definitions */
+export const colors = {
+  darkGreen: "var(--darkGreen)",
+  lightGray: "var(--lightGray)",
+  beige: "var(--beige)",
+  pink: "var(--pink)",
+  orange: "var(--orange)",
+  black: "var(--black)",
+  lightBlue: "var(--lightBlue)",
+  offBlack: "var(--offBlack)"
+};
