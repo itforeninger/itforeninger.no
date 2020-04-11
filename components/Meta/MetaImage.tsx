d3e54cc1ecb410a3886b6b82fa8b212aa394afad
oleast <oleast@stud.ntnu.no>
2020-04-11 12:25:05 +0200

    Add border and background to sharable image

--- components/Meta/MetaImage.tsx
+++ components/Meta/MetaImage.tsx
@@ -7,8 +7,8 @@ export const MetaImage = () => {
     <>
       <meta property="og:image:url" content={CANONICAL_LOGO} />
       <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="1413" />
-      <meta property="og:image:height" content="693" />
+      <meta property="og:image:width" content="1600" />
+      <meta property="og:image:height" content="840" />
       <meta property="og:image:alt" content="FIF logo" />
     </>
   );
