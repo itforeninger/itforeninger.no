5f68adb0b65c51902c3dee89c52d43466ff2d378
oleast <oleast@stud.ntnu.no>
2020-04-11 12:14:04 +0200

    Article iamge url

--- components/Meta/MetaImage.tsx
+++ components/Meta/MetaImage.tsx
@@ -5,7 +5,7 @@ import { CANONICAL_LOGO } from '../../constants/about';
 export const MetaImage = () => {
   return (
     <>
-      <meta property="og:image" content={CANONICAL_LOGO} />
+      <meta property="og:image:url" content={CANONICAL_LOGO} />
       <meta property="og:image:type" content="image/svg+xml" />
       <meta property="og:image:width" content="471" />
       <meta property="og:image:height" content="231" />
